aa11abe206e19e79a9ebda4b7a1ec15d396cd366
iLudens <iaandiaaz@gmail.com>
2023-12-12 21:45:52 +0000

    cambios en BD

--- proyecto_django_parriya/proyecto_django_parriya/settings.py
+++ proyecto_django_parriya/proyecto_django_parriya/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
-import secrets
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,19 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get(
-    "DJANGO_SECRET_KEY",
-    default=secrets.token_urlsafe(nbytes=64),
-)
-IS_HEROKU_APP = "DYNO" in os.environ and not "CI" in os.environ
-# SECURITY WARNING: don't run with debug turned on in production!
-if not IS_HEROKU_APP:
-    DEBUG = True
+SECRET_KEY = 'django-insecure-niyoh2@k*@=t*0aw6gn4xxlma_r+tme01+lfuk^z-%d)+4nply'
 
-if IS_HEROKU_APP:
-    ALLOWED_HOSTS = ["*"]
-else:
-    ALLOWED_HOSTS = []
+# SECURITY WARNING: don't run with debug turned on in production!
+
+
+
+ALLOWED_HOSTS = ['iLudens.pythonanywhere.com']
 
 
 # Application definition
@@ -83,28 +77,19 @@ WSGI_APPLICATION = 'proyecto_django_parriya.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 
-if IS_HEROKU_APP:
-    # In production on Heroku the database configuration is derived from the `DATABASE_URL`
-    # environment variable by the dj-database-url package. `DATABASE_URL` will be set
-    # automatically by Heroku when a database addon is attached to your Heroku app. See:
-    # https://devcenter.heroku.com/articles/provisioning-heroku-postgres
-    # https://github.com/jazzband/dj-database-url
-    DATABASES = {
-        "default": dj_database_url.config(
-            conn_max_age=600,
-            conn_health_checks=True,
-            ssl_require=True,
-        ),
-    }
-else:
+
     # When running locally in development or in CI, a sqlite database file will be used instead
     # to simplify initial setup. Longer term it's recommended to use Postgres locally too.
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
-        }
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "iLudens$parriyappbd",
+        "USER": "iLudens",
+        "PASSWORD": "adminparriyapp",
+        "HOST": "iLudens.mysql.pythonanywhere-services.com",
+        "PORT": 3306,
     }
+}
 
 
 # Password validation
